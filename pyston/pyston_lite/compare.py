ac9707d276e19ca9b79f6f89464c2300a558fda5
Kevin Modzelewski <kevmod@gmail.com>
2022-05-17 17:45:30 -0400

    fixup: testing script

--- pyston/pyston_lite/compare.py
+++ pyston/pyston_lite/compare.py
@@ -12,7 +12,7 @@ Will try to identify the largest differences between the two perf reports
 from collections import defaultdict
 import subprocess
 
-full = defaultdict(int)
+baseline = defaultdict(int)
 lite = defaultdict(int)
 
 def parse(fn):
@@ -29,36 +29,30 @@ def parse(fn):
         r[l[-1]] = int(l[1])
     return r
 
-full = parse("perf_baseline.data")
+baseline = parse("perf_baseline.data")
 lite = parse("perf_lite.data")
 
-full_total = sum(full.values())
+baseline_total = sum(baseline.values())
 lite_total = sum(lite.values())
 
-print("%d\t%d\t" % (full_total, lite_total), "%+.2f%%" % ((lite_total - full_total) / full_total * 100.0))
+print("%d\t%d\t" % (baseline_total, lite_total), "%+.2f%%" % ((lite_total - baseline_total) / baseline_total * 100.0))
 print("="*40)
 
-'''
-full['frame_dealloc'] += full.pop('frame_dealloc_notrashcan', 0)
-full['_PyObject_Free'] += full.pop('PyObject_Free')
-full['unicode_decode_utf8.constprop.0'] += full.pop('unicode_decode_utf8')
-lite["_PyObject_Malloc"] += lite.pop('pymalloc_alloc.isra.0.part.0')
-for d in (lite, full):
-    d["PyObject_RichCompare"] += d.pop("PyObject_RichCompareBool")
-    d["PyObject_GenericGetAttr"] += d.pop("_PyObject_GenericGetAttrWithDict")
-    d["PyObject_GenericSetAttr"] += d.pop("_PyObject_GenericSetAttrWithDict", 0)
-'''
+for d in (lite, baseline):
+    for k, v in list(d.items()):
+        if "EvalFrame" in k:
+            d["_EvalFrame"] += d.pop(k)
 
-all = set(full)
+all = set(baseline)
 all.update(lite)
 
 diffs = []
 for k in all:
-    diffs.append((abs(full.setdefault(k, 0) - lite.setdefault(k, 0)), k))
+    diffs.append((abs(baseline.setdefault(k, 0) - lite.setdefault(k, 0)), k))
 
 diffs.sort(reverse=True)
 for t in diffs[:10]:
     k = t[-1]
-    f = full[k]
+    f = baseline[k]
     l = lite[k]
-    print("%d\t%d\t" % (f, l), "%+.2f%%" % ((l - f) / full_total * 100.0), k)
+    print("%d\t%d\t" % (f, l), "%+.2f%%" % ((l - f) / baseline_total * 100.0), k)
